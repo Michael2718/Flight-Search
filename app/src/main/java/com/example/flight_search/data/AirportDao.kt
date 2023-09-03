a09a78614698df6b6f416c5a061ec5c526fdd462
Michael <isaevmihail007@gmail.com>
2023-09-03 16:55:06 +0300

    Add new Dao methods for database

--- app/src/main/java/com/example/flight_search/data/AirportDao.kt
+++ app/src/main/java/com/example/flight_search/data/AirportDao.kt
@@ -17,6 +17,24 @@ interface AirportDao {
     @Query(
         """
             SELECT * FROM airport
+            WHERE name = :name
+        """
+    )
+    fun getItemByName(name: String): Flow<List<Airport>>
+
+    @Query(
+        """
+            SELECT * FROM airport
+            WHERE iata_code LIKE ('%' || :query || '%') OR name LIKE ('%' || :query || '%')
+            ORDER BY iata_code ASC
+            LIMIT 1
+        """
+    )
+    fun getItemByQuery(query: String): Flow<List<Airport>>
+
+    @Query(
+        """
+            SELECT * FROM airport
             ORDER BY id ASC
         """
     )
@@ -30,4 +48,13 @@ interface AirportDao {
         """
     )
     fun getSuggestions(query: String): Flow<List<Airport>>
+
+    @Query(
+        """
+            SELECT * FROM airport
+            WHERE iata_code <> :departureCode
+            ORDER BY passengers DESC
+        """
+    )
+    fun getDestinations(departureCode: String): Flow<List<Airport>>
 }
--- app/src/main/java/com/example/flight_search/data/FlightSearchRepository.kt
+++ app/src/main/java/com/example/flight_search/data/FlightSearchRepository.kt
@@ -5,9 +5,15 @@ import kotlinx.coroutines.flow.Flow
 interface FlightSearchRepository {
     fun getItemByIataCode(iataCode: String): Flow<List<Airport>>
 
+    fun getItemByName(name: String): Flow<List<Airport>>
+
+    fun getItemByQuery(query: String): Flow<List<Airport>>
+
     fun getAllItems(): Flow<List<Airport>>
 
     fun getSuggestions(query: String): Flow<List<Airport>>
+
+    fun getDestinations(departureCode: String): Flow<List<Airport>>
 }
 
 class LocalFlightSearchRepository(private val dao: AirportDao): FlightSearchRepository {
@@ -16,11 +22,23 @@ class LocalFlightSearchRepository(private val dao: AirportDao): FlightSearchRepo
         return dao.getItemByIataCode(iataCode)
     }
 
+    override fun getItemByName(name: String): Flow<List<Airport>> {
+        return dao.getItemByName(name)
+    }
+
+    override fun getItemByQuery(query: String): Flow<List<Airport>> {
+        return dao.getItemByQuery(query)
+    }
+
     override fun getAllItems(): Flow<List<Airport>> {
         return dao.getAllItems()
     }
 
     override fun getSuggestions(query: String): Flow<List<Airport>> {
         return dao.getSuggestions(query)
+    }
+
+    override fun getDestinations(departureCode: String): Flow<List<Airport>> {
+        return dao.getDestinations(departureCode)
     }
 }
